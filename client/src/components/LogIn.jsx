15a6520912023d19a6db10464c27ce3ce7ed5492
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-31 18:02:37 -0400

    Final Style Touches for Ver 1

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -2,70 +2,101 @@ import React from "react";
 import styled from "styled-components";
 import { WelcomeBtn } from "./Welcome";
 
-
 const LogInForm = styled.form`
-     display: flex;
-    flex-direction: column;
-    margin: 25px auto;
-    width: 90%;
+  display: flex;
+  flex-direction: column;
+  margin: 25px auto;
+  width: 90%;
 
+  h2 {
+    text-align: center;
+    margin: 25px 0px;
+    font-weight: 300;
+  }
 
-    h2 {
-        text-align: center;
-        margin: 25px 0px;
-        font-weight: 300;
-    }
+  input {
+    border-radius: 15px;
+    outline: none;
+    border: none;
+    width: 100%;
+    text-indent: 10px;
+  }
 
+  label {
+    position: relative;
+    font-size: 0.85rem;
+    opacity: 0.65;
+    margin: 8px 0;
+    display: inline-block;
+    transition: .235s;
+  }
 
-
-    input {
-        border-radius: 15px;
-        outline: none;
-        border: none;
-        width: 100%;
-        text-indent: 10px;
-    }
-
-    
-
-    label {
-        position: relative;
-        font-size: .85rem;
-        opacity: .65;
-        margin: 10px 0;
-        display: inline-block;
-    }
-
-    label::before {
-        position: absolute;
-        content: "";
-        width: 100%;
-        background-color: black;
-        bottom: 10%;
-    }
-
-
+  label::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    background-color: black;
+    bottom: 10%;
+  }
+  
 `;
 
 const SignUpBtn = styled(WelcomeBtn)`
-    padding: 3px 10px;
-    margin: 45px;
-    
+  padding: 3px 10px;
+  margin: 45px;
+
+  &:hover {
+        background: #BED4CD;
+        color: white;
+    }
 `;
 
+
+const LabelDiv = styled.div`
+    position: relative;
+    overflow: hidden;
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 50px;
+    height: 1px;
+    background: #528876;
+    top: 25px;
+    transition: .235s;
+    transform: translateX(-100%);
+    opacity: 0;
+    border-radius: 10px;
+  }
+
+  &:hover::before {
+    transform: translateX(5%);
+    opacity: 1;
+  }
+
+  &:hover label {
+    transform: scale(.95);
+    color: #587D87;
+  }
+
+`;
 
 
 export default function LogIn() {
   return (
     <LogInForm>
-                <h2>Welcome back!</h2>
-      <label htmlFor="userName">
-        User Name
-      </label>
-      <input type="text" name="userName" id="userName" required  />
-      <label htmlFor="password">Password </label>
-      <input type="password" name="password" id="password" required />
-      <SignUpBtn type="submit" name="login">Log In</SignUpBtn>
+      <h2>Welcome back!</h2>
+      <LabelDiv>
+        <label htmlFor="userName">User Name</label>
+        <input type="text" name="userName" id="userName" required />
+      </LabelDiv>
+      <LabelDiv>
+        <label htmlFor="password">Password </label>
+        <input type="password" name="password" id="password" required />
+      </LabelDiv>
+      <SignUpBtn type="submit" name="login">
+        Log In
+      </SignUpBtn>
     </LogInForm>
   );
 }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -7,35 +7,34 @@ const UserForm = styled.form`
   max-width: 100%;
   margin: 0 auto;
 
-    h2 {
-        text-align: center;
-        margin: 40px 0px;
-        font-weight: 300;
-    }
+  h2 {
+    text-align: center;
+    margin: 40px 0px;
+    font-weight: 300;
+  }
 
-    input {
-        border-radius: 15px;
-        outline: none;
-        border: none;
-        width: 100%;
-        text-indent: 10px;
-    }
-    label {
-        position: relative;
-        font-size: .75rem;
-        opacity: .65;
-        margin: 5px 0;
-        display: inline-block;
-        
-    }
+  input {
+    border-radius: 15px;
+    outline: none;
+    border: none;
+    width: 100%;
+    text-indent: 10px;
+  }
+  label {
+    position: relative;
+    font-size: 0.75rem;
+    opacity: 0.65;
+    margin: 5px 0;
+    display: inline-block;
+  }
 
-    label::before {
-        position: absolute;
-        content: "";
-        width: 100%;
-        background-color: black;
-        bottom: 10%;
-    }
+  label::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    background-color: black;
+    bottom: 10%;
+  }
 
   .name {
     display: flex;
@@ -44,43 +43,111 @@ const UserForm = styled.form`
 `;
 
 const SignUpBtn = styled(WelcomeBtn)`
-    padding: 3px 10px;
-    margin: 30px;
-    &:hover {
-        background: #BED4CD;
-        color: #222;
-    }
+  padding: 3px 10px;
+  margin: 30px;
+  &:hover {
+    background: #bed4cd;
+    color: #fff;
+  }
 `;
 
+const LabelDiv = styled.div`
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 50px;
+    height: .75px;
+    background: #528876;
+    top: 22px;
+    transition: 0.235s;
+    transform: translateX(-100%);
+    opacity: 0;
+    border-radius: 10px;
+  }
+
+  &:hover::before {
+    transform: translateX(5%);
+    opacity: 1;
+  }
+
+  &:hover label {
+    transform: scale(0.95);
+    color: #587d87;
+  }
+`;
+
+const SpecalLabelDiv = styled(LabelDiv)`
+      position: relative;
+  overflow: hidden;
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 28px;
+    height: .75px;
+    background: #528876;
+    top: 22px;
+    transition: 0.235s;
+    transform: translateX(-100%);
+    opacity: 0;
+    border-radius: 10px;
+  }
+
+  &:hover::before {
+    transform: translateX(5%);
+    opacity: 1;
+  }
+
+  &:hover label {
+    transform: scale(0.95);
+    color: #587d87;
+  }
+
+`
 
 export default function SignUp({}) {
   return (
     <UserForm>
-        <h2>Sign up to start writing your first nuanced note!</h2>
+      <h2>Sign up to start writing your first nuanced note!</h2>
       <div className="name">
         <div className="firstname">
-          <label htmlFor="firstName" name="firstName" id="firstName" required>
-            First Name
-          </label>
-          <input type="text" />
+          <LabelDiv>
+            <label htmlFor="firstName" name="firstName" id="firstName" required>
+              First Name
+            </label>
+            <input type="text" />
+          </LabelDiv>
         </div>
         <div className="lastname">
-          <label htmlFor="lastName" name="lastName" id="lastName" required>
-            Last Name
-          </label>
-          <input type="text" />
+          <LabelDiv>
+            <label htmlFor="lastName" name="lastName" id="lastName" required>
+              Last Name
+            </label>
+            <input type="text" />
+          </LabelDiv>
         </div>
       </div>
-      <label htmlFor="userName" name="userName" id="userName" required>
-        User Name
-      </label>
-      <input type="text" />
-      <label htmlFor="email">Email </label>
-      <input type="email" name="email" id="email" required />
-      <label htmlFor="password">Password </label>
-      <input type="password" name="password" id="password" required />
-      <label htmlFor="password">Confirm Password </label>
-      <input type="password" name="password" id="confirmPassword" required />
+      <LabelDiv>
+        <label htmlFor="userName" name="userName" id="userName" required>
+          User Name
+        </label>
+        <input type="text" />
+      </LabelDiv>
+      <SpecalLabelDiv>
+        <label htmlFor="email">Email </label>
+        <input type="email" name="email" id="email" required />
+      </SpecalLabelDiv>
+      <LabelDiv>
+        <label htmlFor="password">Password </label>
+        <input type="password" name="password" id="password" required />
+      </LabelDiv>
+      <LabelDiv>
+        <label htmlFor="password">Confirm Password </label>
+        <input type="password" name="password" id="confirmPassword" required />
+      </LabelDiv>
       <SignUpBtn type="submit" name="signup">
         Sign Up
       </SignUpBtn>
